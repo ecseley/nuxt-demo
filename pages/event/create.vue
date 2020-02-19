884374916b8a8d131bd721a45aaced9dbaedc214
Endre Cseley <ecseley@jacada.com>
2020-02-19 11:26:34 -0500

    added leftovers from other commit

---
+++ pages/event/create.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Create an Event</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create Event',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Where you can create events'
+        }
+      ]
+    }
+  }
+}
+</script>
