0539b071608189a78948187c6e4ca9b411546176
Endre Cseley <ecseley@jacada.com>
2020-02-19 11:25:34 -0500

    updating demo project with latest exercies

--- pages/index.vue
+++ pages/index.vue
@@ -1,67 +1,40 @@
 <template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        vm-demo
-      </h1>
-      <h2 class="subtitle">
-        VM Nuxt Lessons
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
-        >
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-          >GitHub</a
-        >
-      </div>
-    </div>
-  </section>
+  <div>
+    <h1>Events</h1>
+
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    ></EventCard>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import EventCard from '../components/EventCard'
 
 export default {
+  head() {
+    return {
+      title: 'Event Listing'
+    }
+  },
   components: {
-    Logo
+    EventCard
+  },
+  async asyncData({ $axios, error }) {
+    try {
+      const { data } = await $axios.get('http://localhost:3001/events')
+      return {
+        events: data
+      }
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: 'Unabel to fetch events at this time. Please try agai.'
+      })
+    }
   }
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
